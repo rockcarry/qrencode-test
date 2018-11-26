89f9e73efa0604e5b8c4fcc76516b51d16555539
chenk <chenk@apical.com.cn>
2018-11-26 10:29:58 +0800

    first commit of qrencode-test

---
+++ test.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "qrencode.h"
+
+int main(int argc, char *argv[])
+{
+    QRcode *qr = NULL;
+    unsigned char *data;
+    int x, y;
+
+    if (argc < 2) {
+        printf("usage: qrencode [STRING]");
+        return 0;
+    }
+
+    qr = QRcode_encodeString(argv[0], 0, QR_ECLEVEL_L, QR_MODE_8, 1);
+    printf("version: %d\n", qr->version);
+    printf("width  : %d\n", qr->width  );
+    printf("\n");
+
+    data = qr->data;
+    for (y=0; y<qr->width; y++) {
+        for (x=0; x<qr->width; x++) {
+            printf("%c ", (*data & 1) ? '#' : ' ');
+            data++;
+        }
+        printf("\n");
+    }
+
+    if (qr) QRcode_free(qr);
+}
+
